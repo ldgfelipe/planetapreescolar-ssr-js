92e4e64a5aa775e54e74f3538e61f5818372c11f
ldgfelipe <ldgfelipecarrera@gmail.com>
2022-10-07 16:18:44 -0500

    <update> se crea el ssrapp y se sube a educadorafirebase para probar

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,40 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 const { Nuxt } = require('nuxt-start');
-const nuxtConfig = require('./nuxt.config.js');
-const config={
+const express = require('express');
+const app = express();
+
+//////////// api rest ////////////////
+
+
+app.get("/test", (req, res) => {
+  var json1 = {
+    prueba: "prueba correcta"
+  }
+
+  res.json(json1)
+
+})
+///////////conexion ssrapp///////////
+
+const nuxtConfig = require("./nuxt.config.js");
+
+const config = {
   ...nuxtConfig,
   dev: false,
   debug: false,
-  buildDir: 'nuxt'
+  buildDir: "nuxt",
 };
 const nuxt = new Nuxt(config);
-exports.ssrapp = functions.https.onRequest(async ( req , res ) => {
+
+const ssrapp = functions.https.onRequest(async (req, res) => {
   await nuxt.ready();
-        nuxt.render(req,res);
-})
+  console.log('ini server')
+  nuxt.render(req, res);
+});
+
+const api = functions.https.onRequest(app);
+
+module.exports = {
+  ssrapp,
+  api
+}
